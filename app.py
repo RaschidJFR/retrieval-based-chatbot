129413aac183e614e19a4ac72669437be5a949f9
yuvraj sharma <ysharma@users.noreply.huggingface.co>
2023-07-18 15:19:03 +0000

    create app.py

---
+++ app.py
@@ -0,0 +1,38 @@
+import json 
+import gradio as gr
+import os
+
+
+hf_token = os.getenv('HF_TOKEN')
+api_url = os.getenv('API_URL') 
+headers = {
+    'Content-Type': 'application/json',
+}
+
+system_message = "\nYou are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.\n\nIf a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."
+
+def predict(message, chatbot):
+
+    print(f"message is - {message}")
+    print(f"chatbot is - {chatbot}")
+
+    input_prompt = f"[INST]<<SYS>>\n{system_message}\n<</SYS>>\n\n "
+    for interaction in chatbot:
+        input_prompt = input_prompt + interaction[0] + " [/INST] " + interaction[1] + " </s><s> [INST] "
+
+    input_prompt = input_prompt + message + " [/INST] "
+
+    print(f"input_prompt is - {input_prompt}")
+    data = {
+        "inputs": input_prompt,
+        "parameters": {"max_new_tokens":100}
+    }
+
+    response = requests.post(api_url, headers=headers, data=json.dumps(data), auth=('hf', hf_token))
+
+    print(f'API response is - {response.text}')
+    response_json_object = json.loads(response.text)
+    return response_json_object[0]['generated_text']
+
+
+gr.ChatInterface(predict).queue().launch(debug= True) 
