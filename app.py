c71dfe163a074e2e2b784b67beb27cacf56675e3
yuvraj sharma <ysharma@users.noreply.huggingface.co>
2023-09-11 09:03:55 +0000

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def predict(message, chatbot, system_prompt="", temperature=0.9, max_new_tokens=
     if system_prompt != "":
         system_message = system_prompt
     else:
-        system_message = system_message
+        system_message = "\nYou are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.\n\nIf a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."
         
     temperature = float(temperature)
     if temperature < 1e-2:
@@ -103,7 +103,7 @@ def predict_batch(message, chatbot, system_prompt="", temperature=0.9, max_new_t
     if system_prompt != "":
         system_message = system_prompt
     else:
-        system_message = system_message
+        system_message = "\nYou are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.\n\nIf a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."
         
     temperature = float(temperature)
     if temperature < 1e-2:
